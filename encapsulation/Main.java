120f88b3d6c4aff1aa0f9c7ba74c088405a20765
Dipanshu <dipanshu18881@gmail.com>
2025-07-14 18:16:40 +0530

    Encapsulation & Inheritance

---
+++ encapsulation/Main.java
@@ -0,0 +1,14 @@
+package encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Encapsulation encap = new Encapsulation();
+
+        encap.setId(21);
+        encap.setName("Joe");
+        System.out.println(encap.getId());
+        System.out.println(encap.getName());
+
+    }
+}
---
+++ inheritance/SingleInheritanceA.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class SingleInheritanceA {
+        private int id=1;
+
+        public int getId() {
+            return id;
+        }
+}
+
